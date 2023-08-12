1eb8de8de3de11c9158005adace473b2b88f755b
ntvsp <nguyentuanvuabc@gmail.com>
2023-08-12 23:35:04 +0700

    #

--- assets/js/custome.js
+++ assets/js/custome.js
@@ -88,3 +88,37 @@ $(document).ajaxComplete(function() {
         threshold: 0
     })
 });
+/////////////////////////////////////////////
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    $('a[href*=\\#]:not([href=\\#])').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, function (){
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.evo-tour-program a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.evo-tour-program ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
